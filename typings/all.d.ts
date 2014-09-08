9d5b245eb02d3fa1379fc0913036bd5baa19bff0
Andrew Bradley <cspotcode@gmail.com>
2014-09-08 10:40:11 -0400

    Adds typings to version control

---
+++ typings/all.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="tsd.d.ts" />
+
+// Other definitions not managed by tsd
+/// <reference path="xmldom/xmldom.d.ts" />
+/// <reference path="collections/collections.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
---
+++ typings/xmldom/xmldom.d.ts
@@ -0,0 +1,3 @@
+declare module "xmldom" {
+    var DOMParser: typeof DOMParser;
+}
